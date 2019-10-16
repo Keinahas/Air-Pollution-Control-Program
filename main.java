a48a48f5f63c6ded0c7ad9d5fdb656eceaf37b8a
Keinahas <kaidrielle@gmail.com>
2019-10-16 23:52:16 +0900

    frame created

--- main.java
+++
@@ -1,5 +0,0 @@
-import java.util.*;
-
-public class shangus{
-    System.out.println("노무현은 살아있다!");
-}
---
+++ views/template.java
@@ -0,0 +1,12 @@
+package views;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class template{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
