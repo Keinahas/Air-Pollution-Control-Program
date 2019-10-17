7eb049517c6144956364da0e6d8ca884cc991ef0
Keinahas <kaidrielle@gmail.com>
2019-10-17 13:10:14 +0900

    debugging example

---
+++ views/menubar.java
@@ -0,0 +1,10 @@
+package views;
+
+import javax.swing.JFrame;
+import views.debugging;
+
+public class menubar{
+    public static void main(String[] args) {
+        JFrame frame = new debugging().createFrame();
+    }
+}
