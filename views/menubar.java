9f724948c54126b69d413d36fb2db3ad1ac4be41
Keinahas <kaidrielle@gmail.com>
2019-10-17 19:41:52 +0900

    ez compiler added

--- views/menubar.java
+++ views/menubar.java
@@ -4,6 +4,10 @@ import javax.swing.JFrame;
 import views.debugging;
 
 public class menubar{
+
+    public menubar(){
+        //
+    }
     public static void main(String[] args) {
         JFrame frame = new debugging().createFrame();
     }
