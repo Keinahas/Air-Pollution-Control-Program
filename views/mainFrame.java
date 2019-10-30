e05d828212068b1648cbafccc14b764329424580
chosy0406 <whtjddnjs992@gmail.com>
2019-10-30 18:28:18 +0900

    Add Menubar

--- views/mainFrame.java
+++ views/mainFrame.java
@@ -2,11 +2,16 @@ package views;
 
 import javax.swing.JFrame;
 
-
-public class mainFrame extends JFrame{
-    public mainFrame(int x, int y){
+public class MainFrame extends JFrame{
+    public MainFrame(int x, int y){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(x, y);
-        this.setVisible(true);
+        // this.setVisible(true); 
+        // Watch out for placing setVisible().
+        
+    }
+
+    public void addMenuBar(MyMenubar bar) {
+        this.setJMenuBar(bar);
     }
 }
