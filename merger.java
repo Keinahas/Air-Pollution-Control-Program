6858c2b8acc0d92568b1c657e925ee9f30df1e90
Keinahas <kaidrielle@gmail.com>
2019-11-14 15:49:38 +0900

    added Action Listener & MyMenuItem

--- merger.java
+++ merger.java
@@ -1,15 +1,16 @@
 import javax.swing.JButton;
 import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 
 import views.MyMenubar;
 import views.MyToolBar;
 import views.MainFrame;
+import views.MyMenuItem;
 
 import controls.db.dbActions;
 import controls.csvio.CSVI;
 import controls.triggers.csvOpen;
+import controls.triggers.csvSave;
 
 
 public class Merger{
@@ -34,29 +35,27 @@ public class Merger{
         mb.add(m4);
         mb.add(m5);
 
-        m1.add(new JMenuItem("New"));
-        m1.add(new JMenuItem("Open CSV File"));
-        m1.add(new JMenuItem("Save CSV File"));
-        m1.add(new JMenuItem("Save As..."));
-        m1.add(new JMenuItem("Save Graph As..."));
-        m1.add(new JMenuItem("Exit"));
+        m1.add(new MyMenuItem("New"));
+        m1.add(new MyMenuItem("Open CSV File", new csvOpen()));
+        m1.add(new MyMenuItem("Save CSV File", new csvSave()));
+        m1.add(new MyMenuItem("Save As..."));
+        m1.add(new MyMenuItem("Save Graph As..."));
+        m1.add(new MyMenuItem("Exit"));
 
         //
         
-        m3.add(new JMenuItem("Draw Graph As "));
-        m3.add(new JMenuItem("Draw Graph As "));
-        m3.add(new JMenuItem("Draw Graph As "));
-        m3.add(new JMenuItem("Draw Graph As "));
-        m3.add(new JMenuItem("Draw Graph As "));
-        m3.add(new JMenuItem("Show Concentration"));
+        m3.add(new MyMenuItem("Draw Graph As "));
+        m3.add(new MyMenuItem("Draw Graph As "));
+        m3.add(new MyMenuItem("Draw Graph As "));
+        m3.add(new MyMenuItem("Draw Graph As "));
+        m3.add(new MyMenuItem("Draw Graph As "));
+        m3.add(new MyMenuItem("Show Concentration"));
 
         //
 
-        m5.add(new JMenuItem("Welcome"));
-        m5.add(new JMenuItem("View License"));
-        m5.add(new JMenuItem("About"));
-
-        m1.addActionListener(new csvOpen());
+        m5.add(new MyMenuItem("Welcome"));
+        m5.add(new MyMenuItem("View License"));
+        m5.add(new MyMenuItem("About"));
 
         tb.addTool(new JButton("a"));
         tb.addTool(new JButton("b"));
