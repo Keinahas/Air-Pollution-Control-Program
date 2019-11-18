6ca2092729c2f15df6f016193516726fde23773c
Keinahas <kaidrielle@gmail.com>
2019-11-18 11:20:46 +0900

    createTable test

--- controls/csvio/CSVI.java
+++ controls/csvio/CSVI.java
@@ -6,9 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -31,7 +28,7 @@ public class CSVI {
                 String array[] = line.split(",");
 
                 tmpList = Arrays.asList(array);
-                System.out.println(tmpList);
+                // System.out.println(tmpList);
                 inputList.add(tmpList);
             }
             
