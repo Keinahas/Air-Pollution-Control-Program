617cb81d19621e7a5aa4e8a0b44044b7681c5ed3
Keinahas <kaidrielle@gmail.com>
2019-11-18 11:20:33 +0900

    createTable

--- controls/db/dbActions.java
+++ controls/db/dbActions.java
@@ -9,13 +9,15 @@ import java.sql.Statement;
 
 public class dbActions{
     private Connection conn;
-    dbProperties connectionProps;
+    private dbProperties connectionProps;
 
+    // 생성자
     public dbActions(){
         conn = null;
         connectionProps = new dbProperties();
     }
 
+    // 커낵션 생성
     private void getConnection() throws SQLException {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -26,7 +28,7 @@ public class dbActions{
         return;
     }
 
-    //true: connected, false: not connected
+    // true: connected, false: not connected
     public boolean connect() throws SQLException{
         try{
             getConnection();
@@ -38,10 +40,30 @@ public class dbActions{
         return false;
     }
 
+    // 구문 생성
     public Statement createStatement() throws SQLException{
         Statement st = null;
         st = conn.createStatement();
         return st;
+    }
+
+    // db Create Table
+    public void createTable(String name, String[] args) throws SQLException{
+        int n = args.length;
+        Statement statement = null;
+        String query = "CREATE TABLE " + name + " (\n";
+        for(int i = 0;i < n-1;i++){
+            query += args[i] + " VARCHAR(10),\n";
+        }
+        query += args[n-1] + " VARCHAR(10)\n";
+        query += ");";
+        statement = createStatement();
+        if (statement.executeUpdate(query) >= 0) {
+            System.out.println("CREATED");
+        }else{
+            System.out.println("ERROR");
+        }
+        // System.out.println(str);
     }
 
     public static void main(String[] args) {
@@ -53,11 +75,11 @@ public class dbActions{
             ResultSet rs = null;
             
             statement = db.createStatement();
-            if (statement.execute("SHOW DATABASES")) {
+            if (statement.execute("SHOW TABLES;")) {
             	rs = statement.getResultSet();
             }
 
-            // executeUpdate 함수는 뷰를 업데이트 한다.
+            // executeUpdate 함수는 뷰를 업데이트 한다. (= DDL)
             // if(statement.executeUpdate("SHOW DATABASES"));
             
             while (rs.next()) {
@@ -65,8 +87,12 @@ public class dbActions{
                 System.out.println(str);
             }
 
-            System.out.println(db.conn.getClientInfo());
-            System.out.println(db.conn.toString());
+            // if (statement.executeUpdate("") >= 0) {
+            // 	rs = statement.getResultSet();
+            // }
+            // createTable("일별평균대기오염도_2018","측정일시,측정소명,이산화질소농도,오존농도,이산화탄소농도,아황산가스,미세먼지,초미세먼지".split(","));
+
+            // System.out.println(db.conn.toString());
             db.conn.close();
         }catch(SQLException e){
             e.printStackTrace();
