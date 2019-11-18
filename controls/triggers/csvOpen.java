d40e11b60b50f0c9e6b58cc35c4ed76292113076
Keinahas <kaidrielle@gmail.com>
2019-11-18 11:21:05 +0900

    trigger update

--- controls/triggers/csvOpen.java
+++ controls/triggers/csvOpen.java
@@ -1,18 +1,23 @@
 package controls.triggers;
 
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.util.List;
 
 import controls.csvio.CSVI;
+import controls.db.dbActions;
 
 import java.awt.event.ActionEvent;
 
 public class csvOpen implements ActionListener{
     private CSVI opener;
     private List<List<String>> br;
+    private dbActions db;
+
 
     public csvOpen(){
         super();
+        db = new dbActions();
         opener = new CSVI();
     }
 
@@ -20,5 +25,16 @@ public class csvOpen implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
         br = opener.CSVOpen("일별평균대기오염도_2018.CSV");
+        try{
+            if(db.connect()){
+                db.createTable("일별평균대기오염도_2018",String.join(",", br.get(0)).split(","));
+            }else{
+                System.out.println("Failed");
+            }
+        }
+        catch(SQLException exception){
+            exception.printStackTrace();
+        }
+        // System.out.println(br.get(0));
     }
 }
