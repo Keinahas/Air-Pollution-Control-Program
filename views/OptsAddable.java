41167379102b32a8eb1612b399829142c147f53d
Keinahas <kaidrielle@gmail.com>
2019-11-19 16:33:10 +0900

    Update OptsAddable.java

--- views/OptsAddable.java
+++ views/OptsAddable.java
@@ -4,5 +4,7 @@ import java.awt.event.ActionListener;
 
 public interface OptsAddable{
     public void addOption(String str);
+    public void addOption(String str, int mnemonic);
     public void addOption(String str, ActionListener l);
+    public void addOption(String str, int mnemonic, ActionListener l);
 }
