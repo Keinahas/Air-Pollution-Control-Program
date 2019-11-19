accb488a75c78c3156e5219e9cd39ea43139a833
Keinahas <kaidrielle@gmail.com>
2019-11-19 16:14:38 +0900

    Update MyMenuItem.java

--- views/MyMenuItem.java
+++ views/MyMenuItem.java
@@ -4,12 +4,13 @@ import javax.swing.JMenuItem;
 import java.awt.event.ActionListener;
 
 public class MyMenuItem extends JMenuItem{
-    public MyMenuItem(String arg0){
-        super(arg0);
+
+    public MyMenuItem(String str){
+        super(str);
     }
     
-    public MyMenuItem(String arg0, ActionListener arg1){
-        super(arg0);
-        this.addActionListener(arg1);
+    public MyMenuItem(String str, ActionListener l){
+        super(str);
+        this.addActionListener(l);
     }
 }
