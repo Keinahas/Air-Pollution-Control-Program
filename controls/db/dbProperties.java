5aa782a9213972c788e266db64e4c346033fa5ac
Keinahas <kaidrielle@gmail.com>
2019-11-20 19:14:36 +0900

    Update dbProperties.java
    added comments

--- controls/db/dbProperties.java
+++ controls/db/dbProperties.java
@@ -3,23 +3,26 @@ package controls.db;
 import java.util.Properties;
 
 public class dbProperties extends Properties{
-    private String userName = "root";
-    private String password = "toor";
-    private String serverName = "localhost";
-    private String dbName = "aoop";
-    private int portNumber = 3306;
+    private String userName = "root";           // username
+    private String password = "toor";           // password
+    private String hostAddr = "localhost";      // hostAddr
+    private String dbName = "aoop";             // database
+    private int portNumber = 3306;              // port
 
+    // constructor
     public dbProperties(){
         this.put("user", this.userName);
         this.put("password", this.password);
     }
 
+    // returns dbname
     public String getDBName(){
         return dbName;
     }
 
+    // return Database URL into String
     public String getServerInfo(){
-        return "jdbc:mysql://"+this.serverName +":"+ this.portNumber + "/" + this.dbName+"?characterEncoding=UTF-8&serverTimezone=UTC";
+        return "jdbc:mysql://"+this.hostAddr +":"+ this.portNumber + "/" + this.dbName+"?characterEncoding=UTF-8&serverTimezone=UTC";
         // return "jdbc:mysql://"+this.serverName +":"+ this.portNumber + "/" + this.dbName+"?characterEncoding=UTF-8&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
     }
 }
