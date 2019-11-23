761307c609819ce77b1b6b6f31087d199a284d47
Keinahas <kaidrielle@gmail.com>
2019-11-23 22:35:37 +0900

    Update csvSave.java

--- controls/triggers/csvSave.java
+++ controls/triggers/csvSave.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.sql.SQLException;
 import java.util.List;
 
+import controls.CTRL;
 import controls.csvio.CSVIO;
 import controls.db.dbActions;
 
@@ -13,38 +14,53 @@ import java.awt.event.ActionEvent;
 // class for saving data to csv file || yet coding
 public class csvSave implements ActionListener{
     private CSVIO CSV;
-    private dbActions db;
-    private List<List<String>> br;
 
-    public csvSave(){
-        super();
-        br = null;
-        CSV = new CSVIO();
-        db = new dbActions();
+    public csvSave(CSVIO CSV){
+        this.CSV = CSV;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
         CSV.chooseSave();
+        String name = null;
         File file = CSV.Open();
         if(file == null){
             System.out.println("CSV.Choose() Error!");
             return;
         }
-        try{
-            if(db.connect()){
-                // db.createTable(file.getName().substring(0, file.getName().lastIndexOf(".")), br.get(0).size());
-                // br = db.SelectAllFromTable(name);
-                db.getNStringFromTable(file.getName().substring(0, file.getName().lastIndexOf(".")), 1);
-                System.out.println("csvSave Clicked!");
-            }else{
-                System.out.println("Failed");
-            }
+
+        int ext = file.getName().lastIndexOf(".");
+        if(ext <= 0){
+            name = file.getName() + ".CSV";
+        }else{
+            name = file.getName().substring(0, ext);
         }
-        catch(SQLException exception){
-            exception.printStackTrace();
-        }
-        // CSV.Write(br);
+        System.out.println(name);
+        System.out.println("csvSave Clicked!");
+        // System.out.println(CTRL.getList());
+        // CSV.Write(CTRL.getList());
+
+        // try{
+        //     if(db.connect()){
+        //         int ext = file.getName().lastIndexOf(".");
+                    // if(ext <= 0){
+                    //     System.out.println("EXT ERROR!");
+                    //     return;
+                    // }
+                    // String name = file.getName().substring(0, file.getName().lastIndexOf("."));
+                    // System.out.println(name);
+        //         // db.createTable(file.getName().substring(0, file.getName().lastIndexOf(".")), br.get(0).size());
+        //         br = db.SelectAllFromTable(name);
+        //         // db.getNRowFromTable(file.getName().substring(0, file.getName().lastIndexOf(".")), 1);
+        //         System.out.println("csvSave Clicked!");
+        //         System.out.println(br);
+        //     }else{
+        //         System.out.println("Failed");
+        //     }
+        // }
+        // catch(SQLException exception){
+        //     exception.printStackTrace();
+        // }
     }
 }
