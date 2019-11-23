cc60b9346e95cb86aacf19ffffa5bbec89178b4a
Keinahas <kaidrielle@gmail.com>
2019-11-23 22:36:02 +0900

    Update DrawAction.java

--- controls/triggers/DrawAction.java
+++ controls/triggers/DrawAction.java
@@ -1,37 +1,48 @@
-package views;
+package controls.triggers;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+
+import views.DrawingPanel;
 
 //그래프를 그리는 버튼 ActionListener
 public class DrawAction implements ActionListener {
-	private DrawingPanel drawPanel;
-	private JTextField txt1, txt2, txt3, txt4;
+	private int num;
+	private JTextField txts[];
 	private JComboBox type;
 
-	
-	DrawAction(JTextField txt1, JTextField txt2, JTextField txt3, JTextField txt4, DrawingPanel drawPanel, JComboBox type) { 
-		this.txt1 = txt1;
-		this.txt2 = txt2;
-		this.txt3 = txt3;
-		this.txt4 = txt4;
-		this.drawPanel = drawPanel;
+	public DrawAction(){
+
+	}
+
+	public DrawAction(JComboBox type, JTextField...txtFields) { 
+		this.num = 0;
 		this.type = type;
+		this.txts = txtFields;
+	}
+
+	public void init(JComboBox type, JTextField...txtFields){
+		this.num = 0;
+		this.type = type;
+		this.txts = txtFields;
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		try {
-			int var1 = Integer.parseInt(txt1.getText().trim());
-			int var2 = Integer.parseInt(txt2.getText().trim());
-			int var3 = Integer.parseInt(txt3.getText().trim());
-			int var4 = Integer.parseInt(txt4.getText().trim());
+			int i = 0;
+			int[] vars = new int[num];
+			for (JTextField field : txts) {
+				vars[i++] = Integer.parseInt(field.getText().trim());
+			}
 
-			drawPanel.setScore(var1, var2, var3, var4);
+			((DrawingPanel)(e.getSource())).setScore(vars);
+			// drawPanel.setScore(var1, var2, var3, var4);
 
 			// 그래프를 그리는 패널의 paint()를 간접적으로 호출
-			drawPanel.setGraphType(type.getSelectedIndex());
-			drawPanel.repaint();
+			((DrawingPanel)(e.getSource())).setGraphType(type.getSelectedIndex());
+			((DrawingPanel)(e.getSource())).repaint();
 
 		} catch (NumberFormatException n) {
 			System.out.println("잘못된 숫자 포멧입니다.");
