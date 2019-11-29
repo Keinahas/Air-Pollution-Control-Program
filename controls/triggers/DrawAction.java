6e5261c2e50e312d2433bb2dbf69d77ebedf25f0
Keinahas <kaidrielle@gmail.com>
2019-11-29 21:48:28 +0900

    Update DrawAction.java

--- controls/triggers/DrawAction.java
+++ controls/triggers/DrawAction.java
@@ -9,40 +9,40 @@ import views.DrawingPanel;
 
 //그래프를 그리는 버튼 ActionListener
 public class DrawAction implements ActionListener {
-	private int num;
 	private JTextField txts[];
 	private JComboBox type;
+	private DrawingPanel panel;
 
 	public DrawAction(){
 
 	}
 
-	public DrawAction(JComboBox type, JTextField...txtFields) { 
-		this.num = 0;
+	public DrawAction(DrawingPanel panel, JComboBox type, JTextField...txtFields) { 
 		this.type = type;
 		this.txts = txtFields;
+		this.panel = panel;
 	}
 
-	public void init(JComboBox type, JTextField...txtFields){
-		this.num = 0;
+	public void init(DrawingPanel panel, JComboBox type, JTextField...txtFields){
 		this.type = type;
 		this.txts = txtFields;
+		this.panel = panel;
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		try {
-			int i = 0;
-			int[] vars = new int[num];
-			for (JTextField field : txts) {
-				vars[i++] = Integer.parseInt(field.getText().trim());
+			int[] vars = new int[txts.length];
+
+			//init vars
+			for (int i = 0; i < txts.length; i++) {
+				vars[i] = Integer.parseInt(txts[i].getText().trim());
 			}
 
-			((DrawingPanel)(e.getSource())).setScore(vars);
-			// drawPanel.setScore(var1, var2, var3, var4);
+			panel.setScore(vars);
 
 			// 그래프를 그리는 패널의 paint()를 간접적으로 호출
-			((DrawingPanel)(e.getSource())).setGraphType(type.getSelectedIndex());
-			((DrawingPanel)(e.getSource())).repaint();
+			panel.setGraphType(type.getSelectedIndex());
+			panel.repaint();
 
 		} catch (NumberFormatException n) {
 			System.out.println("잘못된 숫자 포멧입니다.");
