7014b7ad5c6b6f5873b165eaa880de319ef23819
Keinahas <kaidrielle@gmail.com>
2019-12-02 03:30:15 +0900

    Update SideBar.java

--- views/SideBar.java
+++ views/SideBar.java
@@ -9,17 +9,10 @@ import views.MyButton;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.event.ActionListener;
-
-
+import java.util.List;
 import java.awt.Component;
 import java.awt.EventQueue;
-import javax.swing.AbstractCellEditor;
-import javax.swing.JCheckBox;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTree;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeCellEditor;
@@ -27,14 +20,27 @@ import javax.swing.tree.TreeCellRenderer;
 
 public class SideBar extends JPanel {
 
+    private JTree tree;
+    private DefaultTreeModel model;
+
     public SideBar() {
-        
-        JTree tree = createTree();
+        tree = new JTree();
+        setTree();
+        tree.setModel(model);
         tree.setToggleClickCount(0);
         tree.setCellRenderer(new StateRenderer());
         tree.setCellEditor(new StateEditor());
         tree.setEditable(true);
+
+        // DefaultMutableTreeNode root = (DefaultMutableTreeNode)tree.getModel().getRoot();
+
+        // ((StateEditor)tree.getCellEditor()).checkBox.addActionListener( e -> {
+        //     System.out.println(e.getActionCommand());
+        // });
+
         JScrollPane scrollPane = new JScrollPane(tree);
+        scrollPane.setSize(new Dimension(180, 680));
+        scrollPane.setPreferredSize(new Dimension(180, 680));
         this.add(scrollPane);
 
 
@@ -51,17 +57,17 @@ public class SideBar extends JPanel {
         });
     }
 
-    private JTree createTree() {
+    private void setTree() {
         int children = 3;
         int grandChildren = 12;
         DefaultMutableTreeNode root = new DefaultMutableTreeNode(new State("옵션", false));
         DefaultMutableTreeNode node;
 
-        String[] cat = {"측정 일시(월)                                     ","측정 장소","대기 상태"};
+        String[] cat = {"측정 일시(월)","측정 장소","대기 상태"};
         String[][] 옵션 = {
             {"1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"},
             {"강남구","강남대로","강동구","강북구","강서구","공항대로","관악구","광진구","구로구","금천구","노원구","도봉구","도산대로","동대문구","동작구","동작대로","마포구","서대문구","서초구","성동구","성북구","송파구","신촌로","양천구","영등포구","영등포로","용산구","은평구","정릉로","종로","종로구","중구","중랑구","천호대로","청계천로","청계천로","한강대로","홍릉로","화랑로"},
-            {"이산화질소농도(ppm)", "오존농도(ppm)", "이산화탄소농도(ppm)", "아황산가스(ppm)","미세먼지(㎍/㎥)","문세먼지(㎍/㎥)"}
+            {"이산화질소농도(ppm)", "오존농도(ppm)", "이산화탄소농도(ppm)", "아황산가스(ppm)","미세먼지(㎍/㎥)","초미세먼지(㎍/㎥)"}
         };
         for (int j = 0; j < children; j++) {
             node = new DefaultMutableTreeNode(new State(cat[j], false));
@@ -70,8 +76,7 @@ public class SideBar extends JPanel {
                 node.add(new DefaultMutableTreeNode(new State(옵션[j][k], false)));
             }
         }
-        DefaultTreeModel model = new DefaultTreeModel(root);
-        return new JTree(model);
+        model = new DefaultTreeModel(root);
     }
 
     public class State {
@@ -108,11 +113,6 @@ public class SideBar extends JPanel {
         public StateEditor() {
             checkBox = new JCheckBox();
             checkBox.setOpaque(false);
-            checkBox.addActionListener(new ActionListener(){
-                public void actionPerformed(ActionEvent e){
-                    System.out.println(e.getActionCommand());
-                }
-            });
         }
 
         @Override
