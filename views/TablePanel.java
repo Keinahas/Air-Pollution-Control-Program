4b97dce4da49d5c07cf1542042dd1d05e0221cd0
Keinahas <kaidrielle@gmail.com>
2019-12-02 03:30:19 +0900

    Update TablePanel.java

--- views/TablePanel.java
+++ views/TablePanel.java
@@ -1,43 +1,58 @@
 package views;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import java.awt.Dimension;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JTable;
+import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
 
-// import controls.db.dbActions;
-
-public class TablePanel extends JPanel{
+public class TablePanel extends JScrollPane{
+    private JTable table;
     private DefaultTableModel model;
-    private JScrollPane scrollPane;
 
-    public TablePanel(String[] header, String[][] contents){
-        model = new DefaultTableModel(contents, header);
-        JTable table = new JTable(model);
-        scrollPane = new JScrollPane(table);
-        // table.setModel(model);
-        // for (String head : header) {
-        //     model.addColumn(head);
-        // }
-        // for (String[] content : contents) {
-        //     model.addRow(content);
-        // }
-        this.add(scrollPane);
+    public TablePanel(){
+        table = new JTable();
+        this.setViewportView(table);
     }
 
-    public static void main(String[] args) {
-        String[] header = {
-            "측정일시","측정소명","이산화질소농도(ppm)","오존농도(ppm)","이산화탄소농도(ppm)","아황산가스(ppm)","미세먼지(㎍/㎥)","초미세먼지(㎍/㎥)"
-        };
-        String[][] contents = {
-            {"20180101","강남구","0.033","0.01","0.6","0.006","34","22"},
-            {"20180101","강남대로","0.04","0.007","0.8","0.006","","17"},
-            {"20180101","강동구","0.038","0.01","0.7","0.005","48","24"}
-        };
-        JFrame frame = new JFrame("TEST");
-        frame.add(new TablePanel(header, contents));
-        frame.pack();
-        frame.setVisible(true);
+    public void setWholeSize(Dimension d){
+        this.setSize(d);
+        this.setPreferredSize(d);
+    }
+
+    public void setData(List<String> header, List<List<String>> contents){
+        model = new DefaultTableModel();
+        table.setModel(model);
+        if(header == null){
+            System.out.println("header NULL");
+            return;
+        }
+        if(contents == null){
+            System.out.println("contents NULL");
+            return;
+        }
+        for(int i=0;i<header.size();i++){
+            model.addColumn(header.get(i));
+        }
+        for (List<String> content : contents) {
+            String[] strings = new String[header.size()];
+            for(int i=0;i<strings.length;i++){
+                if(i >= content.size()){
+                    strings[i] = "";
+                }else{
+                    strings[i] = content.get(i);
+                }
+            }
+            model.addRow(strings);
+        }
+    }
+
+    public List<List<String>> getData(){
+        List<List<String>> stringLists = new ArrayList<List<String>>();
+
+        return stringLists;
     }
 }
