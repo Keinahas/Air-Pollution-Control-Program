a69d32ed12282e36ca3d8101b8dfb18952fe12fe
Keinahas <kaidrielle@gmail.com>
2019-12-05 10:52:11 +0900

    Update CTRL.java

--- controls/CTRL.java
+++ controls/CTRL.java
@@ -19,6 +19,7 @@ import controls.listeners.tableBelow;
 import controls.listeners.tableNew;
 import javafx.scene.control.ToolBar;
 import views.DrawingPanel;
+import views.GraphPanel;
 import views.MainFrame;
 import views.MyMenuBar;
 import views.MyToolBar;
@@ -34,17 +35,18 @@ public class CTRL{
     private static String fileName = null;
     private static List<String> total = null;
     private static List<String> header = null;
-    private static List<String> average = null;
+    private static List<String> locs = null;
+    private static List<List<String>> average = null;
     private static List<List<String>> contents = null;
 
     ///-------------------------------------------- Vars
-    public static String[] colOpts = {"측정일시(월)", "측정소명", "이산화질소농도(ppm)", "오존농도(ppm)", "이산화탄소농도(ppm)", "아황산가스(ppm)","미세먼지(㎍/㎥)","초미세먼지(㎍/㎥)"};
+    // public static String[] colOpts = {"측정일시(월)", "측정소명", "이산화질소농도(ppm)", "오존농도(ppm)", "이산화탄소농도(ppm)", "아황산가스(ppm)","미세먼지(㎍/㎥)","초미세먼지(㎍/㎥)"};
     public static String[] locOpts = {"강남구","강남대로","강동구","강북구","강서구","공항대로","관악구","광진구","구로구","금천구","노원구","도봉구","도산대로","동대문구","동작구","동작대로","마포구","서대문구","서초구","성동구","성북구","송파구","신촌로","양천구","영등포구","영등포로","용산구","은평구","정릉로","종로","종로구","중구","중랑구","천호대로","청계천로","한강대로","홍릉로","화랑로"};
     public static MainFrame frame = new MainFrame(1500, 800);
     public static MyMenuBar menuBar = new MyMenuBar();
     public static MyToolBar toolBar = new MyToolBar();
     public static SideBar sideBar = new SideBar();
-    public static DrawingPanel drawPanel = new DrawingPanel();
+    public static GraphPanel gPanel = new GraphPanel();
     public static int BARGRAPH = 0;
     public static int LINEGRAPH = 1;
         
@@ -52,7 +54,7 @@ public class CTRL{
     ///-------------------------------------------- PUBLIC
 	public static ActionListener CSV_Save = new csvSave(CSV_IO);
     public static ActionListener CSV_Open = new csvOpen(CSV_IO);
-	public static ActionListener DRAW = new DrawAction();
+	// public static ActionListener DRAW = new DrawAction();
     public static ActionListener DB_Insert = new dbUpload(DB);
     public static ActionListener DB_Show;
     public static ActionListener DB_Drop;
@@ -60,30 +62,14 @@ public class CTRL{
     public static ActionListener T_New_Show = new tableNew();
     public static ActionListener T_Blw_Show = new tableBelow();
     public static ActionListener SelectOpts = new opts(DB);
-    public static ActionListener DrawBarGraph = new DrawAction(CTRL.drawPanel, CTRL.BARGRAPH);
-    public static ActionListener DrawLineGraph = new DrawAction(CTRL.drawPanel, CTRL.LINEGRAPH);
+    public static ActionListener DrawBarGraph = new DrawAction(CTRL.gPanel, CTRL.BARGRAPH);
+    public static ActionListener DrawLineGraph = new DrawAction(CTRL.gPanel, CTRL.LINEGRAPH);
 
     //public static ActionListener BTN_When = ;
     //public static ActionListener BTB_Where = ;
 
 
     ///-------------------------------------------- METHODS
-
-    // public static List<List<String>> getList(){
-    //     return strLists;
-    // }
-    
-    // public static void setList(List<List<String>> tempList){
-    //     strLists = tempList;
-    // }
-
-    // public static List<String> getFileNameList(){
-    //     return fileNameList;
-    // }
-
-    // public static void addFileName(String str){
-    //     fileNameList.add(str);
-    // }
 
     public static List<String> getHeader(){
         return header;
@@ -93,7 +79,7 @@ public class CTRL{
         header = list;
     }
 
-    public static List<String> getAverage(){
+    public static List<List<String>> getAverage(){
         return average;
     }
 
@@ -101,11 +87,13 @@ public class CTRL{
         return total;
     }
 
+
+    ////////
     public static void setAverageNTotal(){
         int contentsNum = 0;
         int lenHeader = header.size();
         double[] total = new double[lenHeader];
-        total[0] = total[1] = 0.0;
+        total[0] = 0.0;
         for(List<String> content : contents){
             int lenContent = content.size();
             for(int i=2;i<lenHeader;i++){
@@ -136,10 +124,10 @@ public class CTRL{
         str += total[total.length-1]/contentsNum;
         list = Arrays.asList(str.split(","));
         System.out.println(list);
-        CTRL.average = list;
+        // CTRL.average = list;
     }
 
-    public static void setAverage(List<String> list){
+    public static void setAverage(List<List<String>> list){
         average = list;
     }
 
@@ -157,11 +145,6 @@ public class CTRL{
 
     public static void setFileName(String str){
         fileName = str;
-    }
-
-    public static ActionListener getGraphAction(DrawingPanel pane, int gType){
-        ((DrawAction)CTRL.DRAW).init(pane, gType);
-        return CTRL.DRAW;
     }
 
     public static String parse(String str){
