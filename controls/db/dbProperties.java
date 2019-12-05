f9eda194a67ebe63ae97a3290b10795c5b3ca678
Keinahas <kaidrielle@gmail.com>
2019-12-05 10:52:05 +0900

    Update dbProperties.java

--- controls/db/dbProperties.java
+++ controls/db/dbProperties.java
@@ -23,7 +23,7 @@ public class dbProperties extends Properties{
 
     // return Database URL into String
     public String getServerInfo(){
-        return "jdbc:mysql://"+this.hostAddr +":"+ this.portNumber + "/" + this.dbName+"?characterEncoding=UTF-8&serverTimezone=UTC";
+        return "jdbc:mysql://"+this.hostAddr +":"+ this.portNumber + "/?characterEncoding=UTF-8&serverTimezone=UTC";
         // return "jdbc:mysql://"+this.serverName +":"+ this.portNumber + "/" + this.dbName+"?characterEncoding=UTF-8&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
     }
 }
