592aa70cd42e848fcbb0d9a9ea648c9017c32efd
Keinahas <kaidrielle@gmail.com>
2019-12-07 15:53:17 +0900

    Update dbUpload.java

--- controls/listeners/dbUpload.java
+++ controls/listeners/dbUpload.java
@@ -12,37 +12,31 @@ import controls.db.dbActions;
 
 // class for saving data to csv file || yet coding
 public class dbUpload implements ActionListener{
-    private dbActions db;
-
-    public dbUpload(dbActions db){
-        this.db = db;
-    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
         if(CTRL.getFileName()==null){
-            JOptionPane aa=new JOptionPane(); 
-            aa.showMessageDialog(null, "Open CSV file first plz.");
+            JOptionPane.showMessageDialog(null, "Open CSV file first plz.");
             return;
         }
         try{
-            if(db.connect()){
-                if(!db.isDataBase("aoop")){
+            if(CTRL.DB.connect()){
+                if(!CTRL.DB.isDataBase("aoop")){
                     int r = JOptionPane.showConfirmDialog(null, "데이터베이스가 없습니다. 새로 만드시겠습니까?", "데이터베이스 생성", JOptionPane.YES_NO_OPTION);
                     if(r == JOptionPane.NO_OPTION)
                         return;
+                    CTRL.DB.createDataBase("aoop");
                 }
-                if(db.isTable(CTRL.getFileName())){
+                if(CTRL.DB.isTable(CTRL.getFileName())){
                     int r = JOptionPane.showConfirmDialog(null, CTRL.getFileName() + "이(가) 이미 있습니다. 새로 만드시겠습니까?", "테이블 생성", JOptionPane.YES_NO_OPTION);
                     if(r == JOptionPane.NO_OPTION){
-                        db.insertIntoTable(CTRL.getFileName(), CTRL.getContents(), CTRL.getHeader().size());
+                        CTRL.DB.insertIntoTable(CTRL.getFileName(), CTRL.getContents(), CTRL.getHeader().size());
                         return;
                     }
-
                 }
-                db.createTable(CTRL.getFileName(), CTRL.getHeader());
-                db.insertIntoTable(CTRL.getFileName(), CTRL.getContents(), CTRL.getHeader().size());
+                CTRL.DB.createTable(CTRL.getFileName(), CTRL.getHeader());
+                CTRL.DB.insertIntoTable(CTRL.getFileName(), CTRL.getContents(), CTRL.getHeader().size());
                 System.out.println("dbUpload Clicked!");
             }else{
                 System.out.println("Failed");
