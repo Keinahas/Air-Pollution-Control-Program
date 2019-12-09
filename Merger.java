10596e1876b175761fd770cb7c84a5051ee3dc09
Keinahas <kaidrielle@gmail.com>
2019-12-10 06:03:21 +0900

    Update Merger.java

--- Merger.java
+++ Merger.java
@@ -35,16 +35,16 @@ public class Merger{
         OptsAddable m1 = new MyMenu("File", KeyEvent.VK_F);
         OptsAddable m2 = new MyMenu("Edit", KeyEvent.VK_E);
         OptsAddable m3 = new MyMenu("View", KeyEvent.VK_V);
-        OptsAddable m4 = new MyMenu("Option", KeyEvent.VK_O);
-        OptsAddable m5 = new MyMenu("Help", KeyEvent.VK_H);
+        // OptsAddable m4 = new MyMenu("Option", KeyEvent.VK_O);
+        // OptsAddable m5 = new MyMenu("Help", KeyEvent.VK_H);
 
         mb.add((MyMenu) m1);
         mb.add((MyMenu) m2);
         mb.add((MyMenu) m3);
-        mb.add((MyMenu) m4);
-        mb.add((MyMenu) m5);
+        // mb.add((MyMenu) m4);
+        // mb.add((MyMenu) m5);
 
-        m1.addOption("New", KeyEvent.VK_N);
+        // m1.addOption("New", KeyEvent.VK_N);
         m1.addOption("Open CSV File", KeyEvent.VK_O, CTRL.CSV_Open);
         m1.addOption("Upload Data", CTRL.DB_Insert);
         m1.addOption("Save CSV File", KeyEvent.VK_S, CTRL.CSV_Save);
@@ -52,14 +52,14 @@ public class Merger{
         m1.addOption("Exit", e->{frame.dispose();System.exit(0);});
 
         m2.addOption("Show New DataTable",CTRL.T_New_Show);
-        m2.addOption("Show DataTable Below", CTRL.T_Blw_Show);
+        // m2.addOption("Show DataTable Below", CTRL.T_Blw_Show);
 
         m3.addOption("DrawBarGraph", CTRL.DrawBarGraph);
         m3.addOption("DrawLineGraph", CTRL.DrawLineGraph);
     
-        m5.addOption("Welcome");
-        m5.addOption("View License");
-        m5.addOption("About");
+        // m5.addOption("Welcome");
+        // m5.addOption("View License");
+        // m5.addOption("About");
 
         tb.addTool(new MyButton(new ImageIcon("./views/resource/open.png"), KeyEvent.VK_O, CTRL.CSV_Open));
         tb.addTool(new MyButton(new ImageIcon("./views/resource/save.png"), KeyEvent.VK_S, CTRL.CSV_Save));
